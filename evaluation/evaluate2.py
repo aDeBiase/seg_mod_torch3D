63003e04f0bf04feb507d48d00fde78c843c9bbc
Alessia De Biase <71633255+aDeBiase@users.noreply.github.com>
2025-01-13 17:10:14 +0100

    Update evaluate2.py

--- evaluation/evaluate2.py
+++ evaluation/evaluate2.py
@@ -72,17 +72,17 @@ def read_split_file(path):
     return dict_from_file
     
 
-path_data = "/data/p302386/hecktor/umcg_data/UMCG_14_17_resampledB/" 
-experiment_folder = '/data/p302386/hecktor/monai_model/experiment_1_new'
-save_files_folder = '/data/p302386/hecktor/monai_model/documents/second_paper/' #'/data/p302386/hecktor/monai_model/ESTRO23/'
+path_data = "/data/DATA_resampled/" 
+experiment_folder = '/data/model/experiment_1'
+save_files_folder = '/data/documents/' 
 segmentation_folder = 'testing_images/testing_images_original'
-multi_view_segmentation_folder = 'reconstructed_volumes/reconstructed_volumes_original'#'reconstructed_volumes'
+multi_view_segmentation_folder = 'reconstructed_volumes/reconstructed_volumes_original'
 
-images_ = os.listdir('/data/p302386/hecktor/monai_model/experiment_1_new/reconstructed_volumes/reconstructed_volumes_original/')
+images_ = os.listdir('/data/model/experiment_1/reconstructed_volumes/reconstructed_volumes_original/')
 patient_IDs = [k.split('.')[0] for k in images_]
 patient_IDs.sort()
 
-original_resolution_file = "/data/p302386/hecktor/monai_model/documents/second_paper/original_resolution_ct.csv"
+original_resolution_file = "/data/documents/original_resolution_ct.csv"
 resolution_df = pd.read_csv(original_resolution_file)
 resolution_df = resolution_df.set_index('PatientID')
 
